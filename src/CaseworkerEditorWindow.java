c58fe00dd526fc33d1d11a9eace23211ecf56a54
L-P-Lewis <lukepiercelewis@gmail.com>
2024-04-29 09:49:38 -0700

    Added editing and creating of caseworkers

--- src/CaseworkerEditorWindow.java
+++ src/CaseworkerEditorWindow.java
@@ -1,5 +1,12 @@
+import lib.Connector;
+
 import javax.swing.*;
 import java.awt.*;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.HashMap;
+import java.util.Map;
 
 public class CaseworkerEditorWindow extends JPanel {
 
@@ -11,13 +18,19 @@ public class CaseworkerEditorWindow extends JPanel {
     JTextField dobField;
     JTextArea certField;
 
+    Map<String, Object> existingCaseworkerData;
+    Map<String, Object> existingPersonData;
+
     public CaseworkerEditorWindow(long caseworkerID) {
+
+
+
         this.setLayout(new BorderLayout());
 
         JPanel topInfoLayout = new JPanel(new FlowLayout(FlowLayout.LEFT));
         topInfoLayout.add(new JLabel("Create or Edit Caseworker"));
         JButton butAddEntry = new JButton("Save");
-        butAddEntry.addActionListener(e -> {});
+        butAddEntry.addActionListener(e -> {addEntry();});
         JButton butBack = new JButton("Back");
         butBack.addActionListener(e -> {Main.previousPanel();});
         topInfoLayout.add(butAddEntry);
@@ -57,9 +70,80 @@ public class CaseworkerEditorWindow extends JPanel {
 
         formContent.add(newPanel);
 
+        if(caseworkerID != -1) {
+            existingPersonData = Connector.getMatchingRows(
+                    "Person", Map.of("ssn", caseworkerID)
+            ).get(0);
+            existingCaseworkerData = Connector.getMatchingRows(
+                    "Caregiver", Map.of("ssn", caseworkerID)
+            ).get(0);
+
+            ssnField.setText(existingPersonData.get("ssn").toString());
+            fNameField.setText(existingPersonData.get("firstName").toString());
+            lNameField.setText(existingPersonData.get("lastName").toString());
+            phNumberField.setText(existingPersonData.get("phoneNumber").toString());
+            emailField.setText(existingPersonData.get("emailAddress").toString());
+            dobField.setText(existingPersonData.get("dateOfBirth").toString());
+            certField.setText(existingCaseworkerData.get("certification").toString());
+
+        }
+
         this.add(formContent, BorderLayout.CENTER);
     }
 
+    private void addEntry() {
+        if (!validateForm()) return;
+        Map<String, Object> newPersonData = compilePersonFormData();
+        Map<String, Object> newCaseworkerData = compileCaseworkerFormData();
+
+        if(existingPersonData != null) {
+            Connector.updateRow("Person", existingPersonData, newPersonData);
+        } else {
+            Connector.insertRow("Person", newPersonData);
+        }
+
+        if(existingCaseworkerData != null) {
+            Connector.updateRow("Caregiver", existingCaseworkerData, newCaseworkerData);
+        } else {
+            Connector.insertRow("Caregiver", newCaseworkerData);
+        }
+
+        Main.previousPanel();
+    }
+
+    // Simple form validation
+    private boolean validateForm() {
+        return (!ssnField.getText().isEmpty()
+                && !fNameField.getText().isEmpty()
+                && !lNameField.getText().isEmpty()
+                && !phNumberField.getText().isEmpty()
+                && !emailField.getText().isEmpty()
+                && !dobField.getText().isEmpty());
+    }
+
+    private Map<String, Object> compilePersonFormData() {
+        DateFormat df = new SimpleDateFormat("yyyy-mm-dd");
+        try {
+            java.sql.Date sqlDate = new java.sql.Date(df.parse(dobField.getText()).getTime());
+            return Map.of(
+                    "ssn", Long.parseLong(ssnField.getText()),
+                    "firstName", fNameField.getText(),
+                    "lastName", lNameField.getText(),
+                    "phoneNumber", phNumberField.getText(),
+                    "emailAddress", emailField.getText(),
+                    "dateOfBirth", sqlDate
+                    );
+        } catch (ParseException e) {
+            return Map.of();
+        }
+    }
+
+    private Map<String, Object> compileCaseworkerFormData() {
+        return Map.of(
+                "ssn", Long.parseLong(ssnField.getText()),
+                "certification", certField.getText()
+        );
+    }
     private JPanel newFormRow(String paramName, JTextField field) {
         JPanel newPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
         newPanel.add(new JLabel(paramName));
